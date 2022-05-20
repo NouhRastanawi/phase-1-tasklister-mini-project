5d04aea29560862967c766f57a901345009e6166
NouhRastanawi <noahrastdark@gmail.com>
2022-05-21 01:23:14 +0300

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  let taskList = document.querySelector("#tasks");
+
+  taskForm.addEventListener("submit", createTask);
 });
+
+function createTask(event) {
+  event.preventDefault();
+
+  const taskDescription = document.getElementById("new-task-description").value;
+
+  let listItem = document.createElement("li");
+  listItem.innerText = taskDescription;
+  document.querySelector("#tasks").appendChild(listItem);
+
+  const button = document.createElement("button");
+  button.innerText = "Remove";
+  listItem.appendChild(button);
+  button.addEventListener("click", removeTask);
+}
+
+function removeTask(event) {
+  event.preventDefault();
+  this.parentElement.remove();
+}
